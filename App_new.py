2b7343bf5439abe11acbdf72bf78a3b8ecfe4954
Layx <lay9412206@gmail.com>
2018-09-12 18:39:51 +0800

    modify

--- App_new.py
+++ App_new.py
@@ -233,5 +233,5 @@ def hello():
 			Title = country_title, Country=Country, Product = Product)
 
 if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port=80)
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
+    # app.run(debug=True)
