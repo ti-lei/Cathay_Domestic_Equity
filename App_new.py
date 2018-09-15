f97387260a443eb7184487ed2c82edea5b8f5c2b
Layx <lay9412206@gmail.com>
2018-09-16 00:02:09 +0800

    modify

--- App_new.py
+++ App_new.py
@@ -549,7 +549,7 @@ def dashboard():
 				Title = country_title, Country=Country, Product = Product)	
 
 if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port=80)
     app.secret_key='secret123'
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
+    # app.run(debug=True)
 
